89cdfa23fdf3d8006d3753267286089d7077f104
Tudor Nechiti <nechititudorr@gmail.com>
2023-06-27 10:44:08 +0200

    First commit, project initialization

---
+++ student-management-system/src/main/java/com/tuds/sss/SimpleStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tuds.sss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStudentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ student-management-system/src/main/java/com/tuds/sss/student/StudentController.java
@@ -0,0 +1,4 @@
+package com.tuds.sss.student;
+
+public class StudentController {
+}
