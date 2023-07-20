d783fb3ebe21c5881719e49a517792d9eeec2b4b
Tudor Nechiti <nechititudorr@gmail.com>
2023-07-20 13:49:06 +0200

    new commit

--- student-management-system/src/main/java/com/tuds/sss/datasource/Datasource.java
+++ student-management-system/src/main/java/com/tuds/sss/datasource/Datasource.java
@@ -1,2 +1,26 @@
-package com.tuds.sss.datasource;public class Datasource {
+package com.tuds.sss.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class Datasource {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource(){
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(HikariDataSource hikariDataSource)
+    {
+        return new JdbcTemplate(hikariDataSource);
+    }
 }
--- student-management-system/src/main/java/com/tuds/sss/student/StudentController.java
+++ student-management-system/src/main/java/com/tuds/sss/student/StudentController.java
@@ -1,4 +1,22 @@
 package com.tuds.sss.student;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
 public class StudentController {
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return List.of(
+                new Student(UUID.randomUUID(), "Tudor", "Nechiti", "nechititudorr@gmail.com", Student.Gender.MALE)
+        );
+    }
 }
